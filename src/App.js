d622793de749703ff7ebf7838954de020ea23ede
Pavlov <pavka@Pavels-MacBook-Pro.local>
2020-06-13 13:31:43 +0300

    created  project and styling the homepage

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
-import './App.css';
+import './homepage.styles.scss';
+
+import HomePage from './homepage';
 
 class App extends Component {
   render() {
-    return <div className='App'>Hello World</div>;
+   
+    return <div className='App'>
+            <HomePage/>
+    </div>;
+    
   }
 }
 
