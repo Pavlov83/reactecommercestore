386904fd03ab9d7fe61442b67f5b98177153a841
Pavlov <pavka@Pavels-MacBook-Pro.local>
2020-06-14 22:56:34 +0300

    added styling to front page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
-import './homepage.styles.scss';
+import './pages/homepage/homepage.styles.scss';
 
-import HomePage from './homepage';
+import HomePage from './pages/homepage/homepage';
 
 class App extends Component {
   render() {
    
     return <div className='App'>
             <HomePage/>
-    </div>;
+           </div>;
     
   }
 }
